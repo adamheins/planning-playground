f59cfcfa817a331fd20d389592b537bb7564ef8c
Adam Heins <mail@adamheins.com>
2022-06-14 15:24:03 -0400

    Add a basic grid search class.

--- planning.py
+++ planning.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import patches
-from scipy.spatial import KDTree
 from pgraph import UGraph
 
 import IPython
@@ -11,7 +10,7 @@ class Rectangle:
     """Rectangular obstacle."""
 
     def __init__(self, xy, width, height):
-        self.xy = xy
+        self.xy = np.array(xy)
         self.width = width
         self.height = height
 
@@ -35,11 +34,18 @@ class Circle:
     """Circular obstacle."""
 
     def __init__(self, xy, radius):
-        self.xy = xy
+        self.xy = np.array(xy)
         self.radius = radius
 
     def draw(self, ax, color="k"):
         ax.add_patch(patches.Circle(self.xy, self.radius, color=color))
+
+    def contains(self, xy):
+        """Check if the point xy is contained in the circle.
+
+        Returns True if the rectangle contains the point, False otherwise.
+        """
+        return np.linalg.norm(self.xy - xy) <= self.radius
 
 
 class Workspace:
@@ -67,12 +73,18 @@ class Workspace:
             obstacle.draw(ax)
 
     def point_is_in_collision(self, xy):
+        """Check if the point xy is in collision with obstacles.
+
+        Returns True if the point is in collision, False otherwise."""
         for obstacle in self.obstacles:
             if obstacle.contains(xy):
                 return True
         return False
 
     def edge_is_in_collision(self, xy1, xy2, h=0.1):
+        """Check if the straight-line edge between points xy1 and xy2 is collision-free.
+
+        The edge is discretized in steps of length h."""
         d = np.linalg.norm(xy2 - xy1)
         r = (xy2 - xy1) / d
 
@@ -107,35 +119,116 @@ class Workspace:
 
 
 class PRM:
+    """Probabilistic road-map for multi-query planning."""
+
     def __init__(self, workspace, n, k):
+        self.workspace = workspace
+        self.graph = UGraph()
+        self.add_vertices(n, k)
+
+    def add_vertices(self, n, k):
+        """Add n new vertices to the graph.
+
+        Vertices are connected to their k nearest neighbours, if the edges
+        between thm are collision-free.
+        """
+        new_size = self.graph.n + n
+
+        while self.graph.n < new_size:
+            # generate a new (collision-free) point
+            point = self.workspace.sample()
+            v = self.graph.add_vertex(point)
+
+            # compute distance to all current points in the graph
+            dists = []
+            for vo in self.graph:
+                if v is vo:
+                    continue
+                dists.append((vo.distance(point), vo))
+            dists.sort(key=lambda x: x[0])
+
+            # add edges between the new vertex and k nearest neighbours if the
+            # edge between them is collision-free
+            for _, vo in dists[:k]:
+                if self.workspace.edge_is_in_collision(v.coord, vo.coord):
+                    continue
+                v.connect(vo)
+
+    def draw(self, ax, vertices=True, edges=True):
+        """Draw the PRM on the provided axes ax."""
+        if vertices:
+            for vertex in self.graph:
+                x, y = vertex.coord
+                ax.plot(x, y, "x", color="b")
+
+        if edges:
+            for edge in self.graph.edges():
+                v1, v2 = edge.endpoints
+                ax.plot(
+                    [v1.coord[0], v2.coord[0]],
+                    [v1.coord[1], v2.coord[1]],
+                    color=(0, 0, 1, 0.25),
+                )
+
+    def query(self, start, goal):
+        """Get the shortest path between the start and goal points, if one exists."""
+        vs, _ = self.graph.closest(start)
+        vg, _ = self.graph.closest(goal)
+        idx, _, _ = self.graph.path_Astar(vs, vg)
+        points = np.array([self.graph[i].coord for i in idx])
+        return np.vstack((start, points, goal))
+
+
+def grid_neighbour_indices(i, j, nx, ny):
+    my_idx = ny * i + j
+    neighbour_idx = []
+
+    # left
+    if i > 0:
+        neighbour_idx.append(ny * (i - 1) + j)
+
+    # lower
+    if j < ny - 1:
+        neighbour_idx.append(ny * i + j + 1)
+
+    # right
+    if i < nx - 1:
+        neighbour_idx.append(ny * (i + 1) + j)
+
+    # upper
+    if j > 0:
+        neighbour_idx.append(ny * i + j - 1)
+
+    return my_idx, neighbour_idx
+
+
+class Grid:
+    def __init__(self, workspace, step):
         self.graph = UGraph()
 
-        # generate samples and add them to the graph
-        self.samples = workspace.sample(n)
-        for sample in self.samples:
-            self.graph.add_vertex(sample)
+        # add a vertex at the corner of every grid cell
+        xs = np.arange(0, workspace.width + step, step) - 0.5 * workspace.width
+        ys = np.arange(0, workspace.height + step, step) - 0.5 * workspace.height
+        for x in xs:
+            for y in ys:
+                self.graph.add_vertex(coord=(x, y))
 
-        # we now want to connect nodes to their k nearest neighbours. We do
-        # this using a KDTree for distance efficient distance computation.
-        tree = KDTree(self.samples)
-        for i, sample in enumerate(self.samples):
-            _, idx = tree.query(sample, k)
-            for j in idx:
-                # don't connect the node to itself
-                if i == j:
-                    continue
+        # connect adjacent vertices
+        nx = xs.shape[0]
+        ny = ys.shape[0]
+        for i in range(nx):
+            for j in range(ny):
+                my_idx, neighbour_idx = grid_neighbour_indices(i, j, nx, ny)
+                v = self.graph[my_idx]
+                for idx in neighbour_idx[:2]:
+                    v.connect(self.graph[idx])
 
-                v1 = self.graph[i]
-                v2 = self.graph[int(j)]
-
-                # don't connect the nodes if the edge between them goes through
-                # an obstacle
-                if workspace.edge_is_in_collision(v1.coord, v2.coord):
-                    continue
-
-                # connect if these nodes are not already connected
-                if not v1.isneighbour(v2):
-                    v1.connect(v2)
+        # remove any vertices in collision
+        # we do this afterward because it is much easier to compute adjacent
+        # nodes for a regular grid rather than one with holes due to obstacles
+        for i in reversed(range(self.graph.n)):
+            if workspace.point_is_in_collision(self.graph[i].coord):
+                self.graph.remove(self.graph[i])
 
     def draw(self, ax, vertices=True, edges=True):
         """Draw the PRM on the provided axes ax."""
@@ -163,7 +256,7 @@ class PRM:
 
 
 def main():
-    # create the workspace
+    # create the workspace with some obstacles
     workspace = Workspace(10, 10)
     workspace.obstacles = [
         Rectangle((0, -2), 1, 3),
@@ -176,14 +269,18 @@ def main():
     start = (-4, -4)
     goal = (4, 4)
 
-    # samples = workspace.sample(10)
-    prm = PRM(workspace, n=100, k=10)
-    path = prm.query(start, goal)
+    # construct PRM and plan a path connecting start to goal
+    # prm = PRM(workspace, n=30, k=10)
+    # path = prm.query(start, goal)
+    grid = Grid(workspace, 0.5)
+    path = grid.query(start, goal)
 
+    # plot the results
     plt.figure()
     ax = plt.gca()
     workspace.draw(ax)
-    prm.draw(ax)
+    # prm.draw(ax)
+    grid.draw(ax)
     ax.plot(start[0], start[1], "o", color="g")
     ax.plot(goal[0], goal[1], "o", color="r")
     ax.plot(path[:, 0], path[:, 1], color="r")
