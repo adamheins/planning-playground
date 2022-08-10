80a65e176f00328a4bb0bba8f72b7f75d8a66efd
michelangelo Fantini <miche@MacBook-Air-di-michelangelo.local>
2022-08-10 15:55:46 -0400

    RRT_star_unbounded

--- RRT_star_unbounded.py
+++ RRT_star_unbounded.py
@@ -1,5 +1,3 @@
-
- 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import patches
@@ -54,9 +52,9 @@ class RRT_star(RRT):
         q = q_new
         if dist < min_edge_len:
             return
-        self.new_point_routine(q,v_nearest)
+        self.new_point_routine(q,v_nearest,dist,max_edge_len)
 
-    def new_point_routine(self, q, v_nearest):
+    def new_point_routine(self, q, v_nearest,dist,max_edge_len):
         n =self.graph.n
         #print(2 * math.sqrt((math.log(n) / n)))
         #rn = min(100/n, 5) # change rn depending on how many nodes there are, look at formula on RTT star paper
@@ -64,7 +62,6 @@ class RRT_star(RRT):
         neighborhood = self.near_vertices(q,rn)
         if len(neighborhood) == 0:
             return
-        v_min = v_nearest
         cost = self.cost(v_nearest.coord) + self.connection(v_nearest.coord, q)
         for vertex in neighborhood:
             point = vertex.coord
@@ -72,35 +69,61 @@ class RRT_star(RRT):
                 c_prime = self.cost(point) + self.connection(point, q)
                 if cost>c_prime:
                     cost=c_prime
-                    v_min = vertex
+                    v_nearest = vertex
+                    dist = self.connection(point, q)
+
+        # split distance into multiple edges
+        if dist > max_edge_len:
+            q_closest = v_nearest.coord
+            vertices = round(dist / max_edge_len)
+            for i in range(vertices - 1):
+                cur_vertex = q_closest + (i + 1) * (q - q_closest) / np.linalg.norm(
+                    q - q_closest
+                )
+                cur_vertex = self.graph.add_vertex(cur_vertex)
+                edge = cur_vertex.connect(v_nearest)
+                cur_vertex.parent = v_nearest
+                self.cost_dic[tuple(cur_vertex.coord)] = self.cost(v_nearest.coord) + self.connection(v_nearest.coord,cur_vertex.coord)
+                cur_vertex.edge = edge
+                v_nearest = cur_vertex
+
+        v = self.graph.add_vertex(q)
+        v.connect(v_nearest)
+        v.parent = v_nearest
         self.cost_dic[tuple(q)] = cost
         v = self.graph.add_vertex(q)
-        edge = v.connect(v_min)
-        v.parent = v_min
+        edge = v.connect(v_nearest)
+        v.parent = v_nearest
         v.edge = edge
-        q_min = v_min.coord
+        q_min = v_nearest.coord
 
+        #reconnect neighboring edges
         for vertex in neighborhood:
             point = vertex.coord
             if np.linalg.norm(point -q_min)== 0:
                 continue
             cost = self.cost(v.coord) + self.connection(q,point)
             if (not self.workspace.edge_is_in_collision(q,point)) and (self.cost(vertex.coord)>cost):
-                self.graph.remove(vertex.edge)
+                try:
+                    self.graph.remove(vertex.edge)
+                except:
+                    print(vertex.coord)
                 vertex.parent = v
                 edge = vertex.connect(v)
                 vertex.edge = edge
                 self.cost_dic[tuple(point)] = cost
         return v
 
-    def end_condition(self, goal, goal_dist=0.5):
+    def end_condition(self, goal, max_edge_len, goal_dist=0.5):
         v_nearest, dist = self.closest_vertex(goal)
         if tuple(goal) in [tuple(i.coord) for i in self.graph]:
+            #print("yes")
             return True 
         elif dist <= goal_dist and not self.workspace.edge_is_in_collision(
             v_nearest.coord, goal
         ):
-            v = self.new_point_routine(goal,v_nearest)
+            print("yes!")
+            v = self.new_point_routine(goal,v_nearest,dist,max_edge_len)
             self.v_goal = v
             return True
         return False
@@ -121,7 +144,7 @@ class RRT_star(RRT):
             # TODO maybe change min and max edge len based on how many points have been taken?
             # add radius with minimum path
             self.expand_graph(min_edge_len, max_edge_len,niu)
-            if self.end_condition(goal,goal_dist=1) and i%30 == 0:
+            if self.end_condition(goal,max_edge_len,goal_dist=1) and i%30 == 0:
                 plt.figure()
                 ax = plt.gca()
                 self.workspace.draw(ax)
